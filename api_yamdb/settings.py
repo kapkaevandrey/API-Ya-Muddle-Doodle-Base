4c3c0ec0c65bb9f11c939f99a9595c90898e8dee
Kapkaev Andrey <kapkaew@yandex.ru>
2021-11-28 21:47:32 +0300

    Second iteration.

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -7,8 +7,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', default="p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+
+DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default="*").split(",")
 
 # Application definition
